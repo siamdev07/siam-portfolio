263188e2f9288a3e150f1fb288c054d751052ff3
Md Siam Hossain <121110174+siamdev07@users.noreply.github.com>
2026-08-09 22:54:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*==================== toggle icon navbar ====================*/
+
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -7,7 +7,7 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
 
-/*==================== scroll sections active link ====================*/
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -26,17 +26,17 @@ window.onscroll = () => {
         };
     });
 
-    /*==================== sticky navbar ====================*/
+    
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
 
-    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+    
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
 
-/*==================== scroll reveal ====================*/
+
 ScrollReveal({
     // reset: true,
     distance: '80px',
@@ -49,7 +49,7 @@ ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact 
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
-/*==================== typed js ====================*/
+
 const typed = new Typed('.multiple-text', {
     strings: ['Frontend Developer', 'Digital Content Creators', 'Videographer'],
     typeSpeed: 100,
